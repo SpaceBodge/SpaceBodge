57c0b33fd82393241b8cee0ec8ffb6e6adef0491
Matthewacon <matthewjbarichello@gmail.com>
2018-04-29 05:38:05 -0400

    Forgot to push gradle config

--- src/org/choose_a_name/main/IPhysicsObject.java
+++ src/org/choose_a_name/main/IPhysicsObject.java
@@ -3,10 +3,18 @@ package org.choose_a_name.main;
 import processing.core.PVector;
 
 public abstract class IPhysicsObject {
- public final PVector pos, av;
- public abstract void update();
- public IPhysicsObject(PVector pos, PVector av) {
+ public final PVector pos, vel, av;
+ public IPhysicsObject(PShape shape, PVector pos, PVector vel, PVector av) {
   this.pos = pos;
+  this.vel = vel;
   this.av = av;
+ }
+ public IPhysicsObject() { this(new PVector(), new PVector(), new PVector()); }
+
+ public void update() {
+  pos.x += vel.x;
+  pos.y += vel.y;
+  pos.z += vel.z;
+
  }
 }
--- src/org/choose_a_name/main/Player.java
+++ src/org/choose_a_name/main/Player.java
@@ -1,19 +1,22 @@
 package org.choose_a_name.main;
 
+import org.choose_a_name.main.shapes.Ship;
 import processing.core.PApplet;
 import processing.core.PShape;
 import processing.core.PVector;
 
-public class Player extends IPshape {
- private PVector pos;
+public final class Player extends IPhysicsObject {
+ public final Ship ship;
 
  public Player(PApplet parentApplet) {
-  super(parentApplet);
+  super();
+  this.ship = new Ship(parentApplet);
  }
 
+ public PVector getPos() { return pos.copy(); }
 
  @Override
- public PShape getShape() {
-  return null;
+ public void update() {
+
  }
 }
